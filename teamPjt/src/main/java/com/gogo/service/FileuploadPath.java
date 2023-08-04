41927427c463c7c3957195fb3b3997a033343ca1
Kimhwapyeong <kimhp0807@naver.com>
2023-08-04 09:22:51 +0900

    fileuploadpath ignore

--- teamPjt/src/main/java/com/gogo/service/FileuploadPath.java
+++
@@ -1,9 +0,0 @@
-package com.gogo.service;
-
-public class FileuploadPath {
-	
-	protected static String dirPath = "D:\\teamProject\\teamProjetGit\\teamPjt\\src\\main\\webapp\\resources\\images\\";
-	//protected static String dirPath = "C:\\Users\\User\\study\\teamProject\\teamPjt\\src\\main\\webapp\\resources\\images\\";
-	
-	
-}
